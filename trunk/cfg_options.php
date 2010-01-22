f26be899b7aadb9e053566991a5120916825d72c
cars@lostroncos.org <cars@lostroncos.org@7babab30-04db-11df-958a-03df8639dc2f>
2010-01-22 06:09:24 +0000

    Fixed small issue with urlde/encode where FolderId wasn't being handled properly. small change to cfg_options to specify url to getappt.php in link in the Atom feed from getfeed.

--- trunk/cfg_options.php
+++ trunk/cfg_options.php
@@ -23,33 +23,42 @@
 #have "Reviewer" permissions on the calendars it will be generating feeds for.
 # used so we don't have to use/keep userids/password for individuals around somewhere.
  
-$cfg_option['user'] = "RSSSVC";
-$cfg_option['password'] = "B@dP@ssw0rd1";
+# To use an account called RSSSvc with a password of FRED1234
+#$cfg_option['user'] = "RSSSVC";
+#$cfg_option['password'] = "FRED1234";
+$cfg_option['user'] = "";
+$cfg_option['password'] = "";
+
 
 #auth method should be either 'basic' or 'NTLM'
 $cfg_option['authmethod'] = 'NTLM';
 
 #Path to the "ported" WSDL file for the Exchange server we're going to talk to. 
-$cfg_option['wsdl'] = "/var/www/ewscalendar/e2k7_wsdl/services.wsdl";
+$cfg_option['wsdl'] = "./e2k7_wsdl/services.wsdl";
 
+# Install Path is path to where files are installed
+#$cfg_option['installpath']="/var/www/ewscalendarfeed";
+$cfg_option['installpath']="/var/www/html";
 
+#URL path - URL Path to the scripts. 
+$cfg_option['urlpath'] = '/ewscalendarfeed';
 
 # Hash of FolderIds and users   ex $PDID['user'] = array(FolderId,ChangeKey)
 #keeping it here is a bad idea for the longterm. may want to move to mySql
 #or SQLite or something in the future
 $PFIDs=array();
 #Testuser's primary/default calendar
-$PFIDs['testuser1'][0]="AAAhAHRlc3R1c2VyMUBleGNoYW5nZS5sb3N0cm9uY29zLm9yZwAuAAAAAABw7Kb1U4UyTbdDaIdxHjy6AQByp0Ud9GkGSYSvTvtebwzmAACRXCcsAAA=";
-$PFIDs['testuser1'][1]="AQAAAA==";
+#$PFIDs['testuser1'][0]="AAAhAHRlc3R1c2VyMUBleGNoYW5nZS5sb3N0cm9uY29zLm9yZwAuAAAAAABw7Kb1U4UyTbdDaIdxHjy6AQByp0Ud9GkGSYSvTvtebwzmAACRXCcsAAA=";
+#$PFIDs['testuser1'][1]="AQAAAA==";
 #ctronco's default calendar
-$PFIDs['ctronco']  [0]="AAAfAGN0cm9uY29AZXhjaGFuZ2UubG9zdHJvbmNvcy5vcmcALgAAAAAATG7V4Of7LUuYb9Ie3YEiPwEAcqdFHfRpBkmEr077Xm8M5gAAkVwnHQAA";
-$PFIDs['ctronco']  [1]="AgAAABYAAAByp0Ud9GkGSYSvTvtebwzmACb6X6XF";
+#$PFIDs['ctronco']  [0]="AAAfAGN0cm9uY29AZXhjaGFuZ2UubG9zdHJvbmNvcy5vcmcALgAAAAAATG7V4Of7LUuYb9Ie3YEiPwEAcqdFHfRpBkmEr077Xm8M5gAAkVwnHQAA";
+#$PFIDs['ctronco']  [1]="AgAAABYAAAByp0Ud9GkGSYSvTvtebwzmACb6X6XF";
 # calendar in ctronco's mailbox called Project Stuff
-$PFIDs['ctronco2'] [0]="AQAfAGN0cm9uY29AZXhjaGFuZ2UubG9zdHJvbmNvcy5vcmcALgAAA0xu1eDn+y1LmG/SHt2BIj8BAHKnRR30aQZJhK9O+15vDOYAKcJJ1M0AAAA=";
-$PFIDs['ctronco2'] [1]="AgAAABYAAAByp0Ud9GkGSYSvTvtebwzmACnCSd3S";
+#$PFIDs['ctronco2'] [0]="AQAfAGN0cm9uY29AZXhjaGFuZ2UubG9zdHJvbmNvcy5vcmcALgAAA0xu1eDn+y1LmG/SHt2BIj8BAHKnRR30aQZJhK9O+15vDOYAKcJJ1M0AAAA=";
+#$PFIDs['ctronco2'] [1]="AgAAABYAAAByp0Ud9GkGSYSvTvtebwzmACnCSd3S";
 # calendar in ctronco's mailbox called Personal Calendar
-$PFIDs['ctronco3'] [0]="AQAfAGN0cm9uY29AZXhjaGFuZ2UubG9zdHJvbmNvcy5vcmcALgAAA0xu1eDn+y1LmG/SHt2BIj8BACF2A4ice5NKqylmrwBU4csAKf+4AAoAAAA=";
-$PFIDs['ctronco3'] [1]="AgAAABQAAAC2/lFJb/EHRL08tHEdy+WCAAAILw==";
+#$PFIDs['ctronco3'] [0]="AQAfAGN0cm9uY29AZXhjaGFuZ2UubG9zdHJvbmNvcy5vcmcALgAAA0xu1eDn+y1LmG/SHt2BIj8BACF2A4ice5NKqylmrwBU4csAKf+4AAoAAAA=";
+#$PFIDs['ctronco3'] [1]="AgAAABQAAAC2/lFJb/EHRL08tHEdy+WCAAAILw==";
 
 
 
